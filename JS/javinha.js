c597581c83218dbcd38398daa24c6865fef8f85b
Guilherme Farias <1801148@SALAS.AULAS>
2018-09-13 22:09:17 -0300

    JS CHAVE

---
+++ JS/javinha.js
@@ -0,0 +1,54 @@
+// Comentario em uma unica linha
+
+/*
+Comentario em varias linha WOOOOOOOOOOOO
+*/
+//var global = fora da função
+//var local =  dentro da função
+//return = return do python, so que com ";" 
+/*
+function spoiler_Alert(){
+    alert("Zezin é top");
+}
+spoiler_Alert();
+
+function spoiler_Alert1(nome, idade){
+    alert(nome + " é top com seus " + idade + " aninhos de pura sabedoria e crack");
+}
+
+spoiler_Alert1("Zezin", "497");
+*/
+/*
+function resultado(n1, n2){
+    var temp = n1 + n2;
+    return temp
+}
+resultado(10, 9)
+*/
+/*
+function resultado1(n01,n02 ){
+    var temp = n01 + n02;
+    if(temp > 0 ){
+        alert("é positivo");
+    }
+    else if(temp < 0){
+        alert("é negativo bam bam");
+    }
+    else{
+        alert("deu 0 filhao");
+    }
+}
+resultado1(10, 0);    
+*/
+/*
+function ateFicarGrandao(){
+    for(i=1; i<11; i++){
+        alert("ZEZIN TA CRESCENDO VIADO " + i);
+    }
+    if (i = 10){
+        alert("SAIUUU DA JAULA BIRLLLL");
+    }
+}
+ateFicarGrandao()
+*/
+
